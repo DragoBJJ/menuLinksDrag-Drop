91a498ddc805413b8b5e78f382c24337039e2aa3
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-12-05 16:35:32 +0100

    Rename MenuItemProps to MenuLinksWrapperProps for clarity in LinksWrapper component

--- src/components/molecules/linksWrapper/LinksWrapper.tsx
+++ src/components/molecules/linksWrapper/LinksWrapper.tsx
@@ -1,11 +1,11 @@
 import { memo, PropsWithChildren } from 'react';
 import { Button } from '../../atoms/Button/Button';
 
-type MenuItemProps = {
+type MenuLinksWrapperProps = {
   setAddLinkAction: () => void;
 } & Required<PropsWithChildren>;
 
-export const LinksWrapper = memo<MenuItemProps>(({ children, setAddLinkAction }) => {
+export const LinksWrapper = memo<MenuLinksWrapperProps>(({ children, setAddLinkAction }) => {
   return (
     <div className="flex w-full flex-col overflow-hidden rounded-md border-[1px] border-primary bg-white">
       <div className="w-ful h-full bg-secondary">{children}</div>
