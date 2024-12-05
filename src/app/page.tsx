03acc2c6aa6f6918762ae34279fa79baabcf7955
Jakub Pawelski <drago@Jakubs-MacBook-Pro.local>
2024-12-05 13:34:36 +0100

    Moving NavLinksProvider to NavigationLinksTemplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { NavigationLinksTemplate } from '../components/templates/NavigationLinksTemplate';
-import { NavLinksProvider } from '../context/useNavLinksContext';
 
 export default function Home() {
   return (
     <div className="m-auto flex min-h-[100vh] w-full max-w-[100vw] items-start justify-center bg-white p-20">
-      <NavLinksProvider>
-        <NavigationLinksTemplate />
-      </NavLinksProvider>
+      <NavigationLinksTemplate />
     </div>
   );
 }
--- src/components/templates/NavigationLinksTemplate.tsx
+++ src/components/templates/NavigationLinksTemplate.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { NavLinksProvider } from '../../context/useNavLinksContext';
 import { useLinkAction } from '../../hooks/useLinkAction';
 import { LinksMenu } from '../molecules/LinksMenu/LinksMenu';
 import { NavigationForm } from '../organisms/NavigationForm/NavigationForm';
@@ -8,11 +9,13 @@ export const NavigationLinksTemplate = () => {
   const { linkAction, setAddLinkAction, setOffLinkAction } = useLinkAction();
 
   return (
-    <div className="flex h-full min-h-[560px] w-full flex-col items-center justify-start border-[1px] border-dotted border-third-border-purple p-6">
-      <div className="flex h-full w-full flex-col items-center justify-center gap-l">
-        <LinksMenu setAddLinkAction={setAddLinkAction} />
-        {linkAction === 'ADD' && <NavigationForm setOffLinkAction={setOffLinkAction} />}
+    <NavLinksProvider>
+      <div className="flex h-full min-h-[560px] w-full flex-col items-center justify-start border-[1px] border-dotted border-third-border-purple p-6">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-l">
+          <LinksMenu setAddLinkAction={setAddLinkAction} />
+          {linkAction === 'ADD' && <NavigationForm setOffLinkAction={setOffLinkAction} />}
+        </div>
       </div>
-    </div>
+    </NavLinksProvider>
   );
 };
